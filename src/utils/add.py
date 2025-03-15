0a994bc00e722ab3b50ff44f5ec35ad2a572950e
mulandi <mulandimorris1@gmail.com>
2025-03-15 19:13:20 +0300

    still adding add.py

--- src/utils/add.py
+++ src/utils/add.py
@@ -8,27 +8,29 @@ from rich import print as printc
 
 
 def computeMasterKey(mp, ds):
- password = mp.encode()
- salt = ds.encode()
- key = PBKDF2(password, salt, 32, count=1000000, hmac_hash_module=SHA512)
- return key
+	password = mp.encode()
+	salt = ds.encode()
+	key = PBKDF2(password, salt, 32, count=1000000, hmac_hash_module=SHA512)
+	return key
 
 
 def addEntry(mp, ds, sitename, siteurl, email, username):
- #get the password
- password = getpass("Enter password: ")
+	# get the password
+	password = getpass("Enter password: ")
 
- mk = computeMasterKey(mp, ds)
+	mk = computeMasterKey(mp, ds)
 
- encrypted = utils.encrypt(key=mk, source=password, keyType='bytes')
+	encrypted = utils.aesutil.encrypt(key=mk, source=password, keyType='bytes')
 
- #add to db
+	# add to db
 
- db = dbconfig()
- cursor = db.cursor()
- query = "INSERT INTO password_manager.entries (sitename, siteurl, email, username, password) VALUES (%s, %s, %s, %s, %s)"
- val = (sitename, siteurl, email, username, encrypted)
- cursor.execute(query,val)
- db.commit()
+	db = dbconfig()
+	cursor = db.cursor()
+	query = ("INSERT INTO password_manager.entries "
+			 "(sitename, siteurl, email, username, password) "
+			 "VALUES (%s, %s, %s, %s, %s)")
+	val = (sitename, siteurl, email, username, encrypted)
+	cursor.execute(query, val)
+	db.commit()
 
- printc("[green][+] Entry added successfully[/green]")
+	printc("[green][+] Entry added successfully[/green]")
